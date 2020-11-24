ef65e8628f763f618b50849a6cdb73340b03e2c6
Jarom Rial <jarom.rial@gmail.com>
2020-11-24 13:37:38 -0700

    chore: setup tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-postcss',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
