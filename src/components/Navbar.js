7d9a294bc8408eda68cc54f172bfa3db240710a6
Jarom Rial <jarom.rial@gmail.com>
2021-01-02 06:06:05 -0700

    task: change from 7xl to 8xl maxWidth

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <div className="fixed w-full z-50">
             <nav className="bg-gray-50">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16">
                         <div className="flex items-center">
                             <div className="flex-shrink-0">
