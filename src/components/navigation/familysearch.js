33c14074251d692998f193bfc52c1eda55183683
Jarom Rial <jarom.rial@gmail.com>
2021-01-02 15:53:49 -0700

    task: add sidenav to familysearch

---
+++ src/components/navigation/familysearch.js
@@ -0,0 +1,344 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const FamilySearchNav = () => {
+    return (
+        <nav className="px-1 overflow-y-auto font-medium text-base lg:text-sm pb-10 lg:pb-14 sticky?lg:h-(screen-18)">
+            <ul>
+                <li className="mt-2">
+                    <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Overview</Link>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Family Tree
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Navigate</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">View Details</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Children &amp; Spouses</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Find &amp; Recents</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Alternate Views</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Change Info</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Add Ancestors</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Add Sources</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Find &amp; Attach Records</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Notes</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Merging</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Charts &amp; Forms</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Account</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Solutions Gallery</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Memories
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Gather Your Records</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Gathering Records from Others</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Interview Relatives</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Browse Memories</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Prepare Uploads</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Add &amp; Tag Photos</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Add Documents</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Add Stories</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Write a Life Sketch</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Add Audio Files</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Use the Gallery</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Solutions Gallery</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Descendants
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Identify Lines</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Sourcing Tools</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Puzzilla</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Relative Finder</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Discover
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Get Organized</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Identify a Line</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Form a Strategy</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">FamilySearch Record Searches</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">FamilySearch Research Tools</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Develop Search Skills</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Solve Problems</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">FamilySearch Places &amp; Solutions Gallery</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Where Am I From?</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Goldie May</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Indexing
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Learn About Indexing</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Get Started</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Index Batches</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Improve Skills</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Collaborate With Others</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Read Difficult Handwriting</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Help
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Get Help</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Genealogical Societies</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Collaborate</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Help Another Person</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Publish a Family History</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">O-Volunteer</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">JAWS</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        Technology
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Webinars</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Tablets and Smartphones</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Social Media</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Genealogy Software</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Create and Upload a Family Tree</Link>
+                        </li>
+                    </ul>
+                </li>
+
+                <li className="mt-8">
+                    <h5 className="mb-3 lg:mb-3 uppercase tracking-wide font-semibold text-sm lg:text-xs text-gray-900">
+                        DNA
+                    </h5>
+
+                    <ul>
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Basic Genetics &amp; Research</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Learn About DNA Testing</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Take a DNA Test</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">DNA Matching</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Ethnicity &amp; Relationships</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">DNA Research Tools</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Adoption &amp; DNA</Link>
+                        </li>
+
+                        <li>
+                            <Link className="py-2 transition-colors duration-200 relative block hover:text-gray-900 text-gray-500" to="#">Help With Questions</Link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default FamilySearchNav
--- src/templates/familysearch.js
+++ src/templates/familysearch.js
@@ -3,20 +3,33 @@ import PropTypes from "prop-types"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Content, { HTMLContent } from "../components/Content"
+import FamilySearchNav from "../components/navigation/familysearch"
 
 export const FamilySearchTemplate = ({ title, category, content, contentComponent }) => {
     const PageContent = contentComponent || Content
 
-    return (
-        <section className="bg-gray-50 py-16 overflow-hidden">
-            <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
-                <div className="text-base max-w-prose mx-auto lg:max-w-none">
-                    <h2 className="text-base text-green-600 font-semibold tracking-wide uppercase">{category}</h2>
-                    <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{title}</h3>
+    return (<>
+        <div className="w-full max-w-8xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+            <div className="mt-16 lg:flex">
+                <div className="fixed z-40 inset-0 flex-none h-full bg-black bg-opacity-25 w-full lg:bg-transparent lg:static lg:h-auto lg:overflow-y-visible lg:pt-0 lg:w-60 xl:w-72 lg:block hidden">
+                    <div className="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:sticky lg:bg-transparent overflow-hidden lg:top-18 mr-24 lg:mr-0">
+                        <h2 className="text-base text-green-600 font-semibold tracking-wide uppercase">{title}</h2>
+                        <FamilySearchNav />
+                    </div>
+                </div>
+                <div className="min-w-0 w-full flex-auto lg:static lg:max-h-full lg:overflow-visible">
+                    <div className="text-base max-w-prose mx-auto lg:max-w-none">
+                        <div className="max-w-3xl">
+                            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-6xl sm:leading-tight lg:inline">
+                                Welcome to
+                                <span className="text-green-700 px-3">{title}</span>
+                            </h3>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
-    )
+        </div>
+    </>)
 }
 
 FamilySearchTemplate.propTypes = {
@@ -28,7 +41,7 @@ FamilySearchTemplate.propTypes = {
 
 const FamilySearchPage = ({ data }) => {
     const { markdownRemark: post } = data
-    
+
     return (
         <Layout>
             <FamilySearchTemplate
