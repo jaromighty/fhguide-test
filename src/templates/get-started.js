add359dd9c09b4da218611709abf2efa7ec14d22
Jarom Rial <jarom.rial@gmail.com>
2021-01-01 17:05:34 -0700

    task: updates to get started page

--- src/templates/get-started.js
+++ src/templates/get-started.js
@@ -8,20 +8,26 @@ export const GetStartedTemplate = ({ title, content, contentComponent }) => {
     const PageContent = contentComponent || Content
 
     return (
-        <div className="bg-gray-50 overflow-hidden">
-            <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-                <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
-                <div className="mx-auto text-base max-w-prose lg:max-w-none">
-                    <h2 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <div className="bg-gray-50 py-16 overflow-hidden">
+            <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+                <div className="text-base max-w-prose mx-auto lg:max-w-none">
+                    <h2 className="text-base text-green-600 font-semibold tracking-wide uppercase">
                         {title}
                     </h2>
                 </div>
-                <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
-                    <div className="relative lg:row-start-1 lg:col-start-2">
-                        {/* Image column */}
+                <div className="lg:grid lg:grid-cols-3 lg:gap-8 lg:items-start">
+                    <div className="relative col-span-2 z-10">
+                        <PageContent className="prose prose-cyan text-gray-500 mx-auto lg:max-w-none" content={content} />
                     </div>
-                    <div className="mt-8 lg:mt-0">
-                        <PageContent className="mt-5 prose text-gray-500 mx-auto lg:max-w-none" content={content} />
+                    <div className="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
+                        <svg className="absolute top-0 right-0 -mt-20 -mr-20 lg:top-auto lg:right-auto lg:bottom-1/2 lg:left-1/2 lg:mt-0 lg:mr-0 xl:top-0 xl:right-0 xl:-mt-20 xl:-mr-20" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+                            <defs>
+                                <pattern id="bedc54bc-7371-44a2-a2bc-dc68d819ae60" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                    <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor" />
+                                </pattern>
+                            </defs>
+                            <rect width="404" height="384" fill="url(#bedc54bc-7371-44a2-a2bc-dc68d819ae60)" />
+                        </svg>
                     </div>
                 </div>
             </div>
